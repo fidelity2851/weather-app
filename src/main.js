727724ead28ba1dc6e27ee367221aa3712ba0b8b
fidelity2851 <46867012+fidelity2851@users.noreply.github.com>
2023-03-08 10:50:47 +0100

    Fixed an HTTPS bug in my main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://api.weatherapi.com/v1/';
+axios.defaults.baseURL = 'https://api.weatherapi.com/v1/';
 
 createApp(App).use(router).mount('#app')
